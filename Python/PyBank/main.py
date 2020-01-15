d7747bd5e19d64c4dddf0781b88a061e66e3d96e
Brian-Perry <blperry5@gmail.com>
2020-01-15 14:59:13 -0800

    Comments added

--- Python/PyBank/main.py
+++ Python/PyBank/main.py
@@ -1,6 +1,7 @@
 import os
 import csv
 
+# create and set variables for profit/loss, total months, greatest increase, greatestest decrease, average change, and previous profit/loss
 pltotal = 0
 months = 0
 gincrease = 0
@@ -8,45 +9,48 @@ gdecrease = 0
 avechange = 0
 prevpl = 0
 
+# set the path of the dataset.
 csvpath = os.path.join("Resources", "budget_data.csv")
 
+# open the csvfile
 with open(csvpath, newline='') as csvfile:
-
+    # use the reader function to iterate through the file.
     csvreader = csv.reader(csvfile, delimiter=',')
-
+    # store the header into cvsheader and move to the next line.
     csvheader = next(csvreader)
-
+    # iterate through the file
     for row in csvreader:
+        # add each line to get the total of months
         months += 1
-        
+        # add all the profit/loss totals
         pltotal += int(row[1])
-        
+        # find the difference by taking the current profit/loss subracted by the previous profit/loss
         diff = int(row[1]) - prevpl
-        
+        # calculate the average change only if the previous profit/loss wasn't zero.
         if prevpl != 0:
             avechange += diff
-
+        # checks to see if the new difference is bigger then any previous difference.  If it is, then it is the new greatest increase.
         if diff > gincrease:
             gincrease = diff
             imonth = row[0]
-        
+        # checks to see if the enew difference is less then any previous difference.  If it is, then it is the new lowest decrease.
         if diff < gdecrease:
             gdecrease = diff
             dmonth = row[0]
-        
+        # sets the previous profit/loss variable to check against the new profit/loss on the next row.
         prevpl = int(row[1])
-
+# calculates the average change by dividing the average change by how many months there are minus 1 to acount for the first extry with no difference.
 avechange = (avechange / (months - 1))
-
+# prints all the output to "bankpyout.txt".
 with open('bankpyout.txt', 'w', newline='') as output:
     output.write('Financial Analysis\n')
     output.write(('-' * 80) + '\n')
     output.write(f'Total Months: {months}\n')
     output.write(f'Total: ${pltotal}\n')
-    output.write("Average Change: $" + "{0:.2f}\n".format(avechange,  2))
+    output.write(f"Average Change: $  {format(avechange, '.2%')}\'n")
     output.write(f'Greatest Increase in Profits: {imonth} (${gincrease})\n')
     output.write(f'Greatest Decrease in Profits: {dmonth} (${gdecrease})\n')
-
+# prints all the output to the console.
 print('Financial Analysis')
 print("-" * 80)
 print(f'Total Months: {months}')
--- Python/PyPoll/main.py
+++ Python/PyPoll/main.py
@@ -1,37 +1,47 @@
 import os
 import csv
 
+#set path of data set
 csvpath = os.path.join("Resources", "election_data.csv")
 
+#create an empty list to store canidate names into.
 canidate = []
+#create an empty list to store how many votes each canidate recieves.
 vote_count = []
+#create and set our total vote count vairable to zero.
 totalvote = 0
 
+# Open the csv as the object csvfile.
 with open(csvpath, newline="") as csvfile:
-    
+    #use the reader fuction to itirate through the csv file with csvreader.
     csvreader = csv.reader(csvfile, delimiter=',')
+    # Store the header into cvsheader and move to the next line in csvreader.
     csvheader = next(csvfile)
-
+    #itorate through the whole csv file, count total votes.
     for row in csvreader:
         totalvote += 1
         
+        # check if canidate name isn't in the list of canidates, add their name to it and add 1 vote in the vote count list.
         if row[2] not in canidate:
             canidate.append(row[2])
             vote_count.append(1)
-
+        #if their name is in the list, find where in the index their name is and add a vote to the corresponding index.
         else: vote_count[canidate.index(row[2])] += 1
 
+#Write output to pypollout.txt with proper formatting.
 with open('pypollout.txt', 'w', newline='') as output:
     output.write(f'Election Resultsn\n') 
     output.write('-' * 80 + '\n')
     output.write(f'Total Votes: {totalvote}\n')
     output.write('-' * 80 + '\n')
+    #find how many canidates were found in the file and print them all out with proper stats and formatting.
     for name in range(len(canidate)):
         output.write(f'{canidate[name]}: {format(vote_count[name] / totalvote, ".3%")} ({vote_count[name]})\n')
     output.write('-' * 80 + '\n')
     output.write(f'Winner: {canidate[vote_count.index(max(vote_count))]}\n')
     output.write('-' * 80 + '\n')
 
+#prints same results to the console instead of the pypollout.txt file.
 print(f'Election Results')
 print('-' * 80)
 print(f'Total Votes: {totalvote}')
